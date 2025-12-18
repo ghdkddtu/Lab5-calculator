3ea98279e6661cf7ce55f4d72a058d54f53e97bb
Irina <keilthera@4yandex.ru>
2025-12-19 00:34:39 +0400

    Добавлен калькулятор с сложением и вычитанием

---
+++ calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+message = '''
+Выберите математическую операцию:
++ : Сложение
+- : Вычитание
+
+Ваш выбор: '''
+
+operation = input(message)
+
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+else:
+    print('Неизвестная операция')
+    exit()
+
+print("Результат:", result)
